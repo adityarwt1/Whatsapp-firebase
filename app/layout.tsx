102dc3ec8362ae9cb6dd41e109dc1b1e7465aeb6
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-10-17 14:15:27 +0530

    done the isonline part

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
-
+import IsOnline from "@/components/IsOnline";
 export const metadata: Metadata = {
   title: "What's App Clone - Aditya Rawat",
   description: "What's App Clone. Nextjs Firebase",
@@ -23,6 +23,7 @@ export default function RootLayout({
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={<div>Loading...</div>}>
           <Toaster />
+          <IsOnline />
           {children}
           <Analytics />
         </Suspense>
--- contexts/IsOnline.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createContext } from "react";
-
-interface isOnline {
-  isOnline: boolean;
-}
-
-export const isOnline = createContext<isOnline>();
